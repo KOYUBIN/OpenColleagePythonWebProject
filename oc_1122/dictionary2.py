7874f4bb35aa36eb9c42073830e9a6dd50cf6dba
고유빈 <gjsxjzlffj96@gmail.com>
2019-11-22 21:21:05 +0900

    - forloop.py 생성

---
+++ oc_1122/dictionary2.py
@@ -0,0 +1,9 @@
+menu = {"라면" : 4000, "만두" : 3500, "보쌈" : 28000}
+while True:
+    choice= input("메뉴를 입력하세요 : ")
+
+    if choice in menu:
+        price = menu[choice]
+        print(choice + "는 " + str(price) + "원 입니다.")
+    else:
+        print(choice + " 메뉴는 없습니다")
